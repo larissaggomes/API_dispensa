a851989e2bbdb4c70cdf229a710ba71d742ebaeb
larissaggomes <larissgomes92@gmail.com>
2023-12-06 15:55:43 -0400

    feat: adicionar ler

--- app.py
+++ app.py
@@ -2,24 +2,30 @@ from flask import Flask, Response, jsonify, request, json
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/foo": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-type'
+cors = CORS(app)
 
-#consultar(todos)
-@app.route('/produtos', methods=['GET'])
-@cross_origin(origin='+',headers=['Content- Type','Authorization'])
-def obter_produto():
+#lista de produtos
+@app.route('/internal')
+def ler():
     try:
         with open("teste.json", "r") as arquivo:
             json_objeto = json.load(arquivo)
         return json_objeto
     except IOError:
-        return []   
+        return [] 
+
+#consultar(todos)
+@app.route('/produtos', methods=['GET'])
+def obter_produtos():
+    resultado = ler()
+
+    return Response(
+        response=json.dumps(resultado), status=200,  mimetype="text/plain")
     
 #consultar(por id)
 @app.route('/produtos/<int:id>',methods=['GET'])
 def obter_produto_por_id(id):
-    resultado = obter_produto()
+    resultado = ler()
     for produto in resultado:
         if produto["id"] == id:
             return produto  
@@ -28,7 +34,7 @@ def obter_produto_por_id(id):
 @app.route('/produtos/<int:id>',methods=['PUT'])
 def editar_produto_por_id(id):
     produto_alterado = request.get_json()
-    resultado = obter_produto()
+    resultado = ler()
     for produto in resultado:
         if produto["id"] == id:
             produto["nome"] = produto_alterado["nome"]
@@ -40,22 +46,24 @@ def editar_produto_por_id(id):
         return produto_alterado
     
 #criar 
-@app.route('/produtos',methods=['POST'])
-@cross_origin()        
+@app.route('/produtos',methods=['POST'])    
 def incluir_novo_produto():
     novo_produto = request.get_json()
-    resultado = obter_produto()
+    resultado = ler()
     resultado.append(novo_produto)
     json_objeto = json.dumps(resultado, indent=4)
 
     with open("teste.json", "w") as arquivo:
         arquivo.write(json_objeto)
-        return Response(content_type=True, status=200)
+    
+    
+    return Response(
+        response=json.dumps(True), status=200,  mimetype="text/plain")
     
 #excluir
 @app.route('/produtos/<int:id>',methods=['DELETE'])
 def excluir_produto(id):
-    resultado = obter_produto()
+    resultado = ler()
     for indice,produto in enumerate(resultado):
         if produto["id"] == id:
             del resultado[indice]
